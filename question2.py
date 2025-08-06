f8c5694c9a0bac4af49a44a07a47b8f31c2c7a46
Daniel Muhia <muhiadan17@gmail.com>
2025-08-06 19:15:41 +0300

    modifies repo

--- question2.py
+++ question2.py
@@ -1,16 +1,15 @@
 #function, that returns the first non-repeated character in the given string.
-def first_non_repeated_character(s):
-  char_count = {}
-
-  for char in s:
-    if char in char_count:
-      char_count[char] += 1
-    else:
-      char_count[char] = 1
-  
-  for char in s:
-    if char_count[char] == 1:
-      return char
-  
-  return None
+def first_non_repeated(s):
+    char_count = {}
+    
+    for char in s:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+            
+    for char in s:
+        if char_count[char] == 1: 
+            return char
+    return None
 
